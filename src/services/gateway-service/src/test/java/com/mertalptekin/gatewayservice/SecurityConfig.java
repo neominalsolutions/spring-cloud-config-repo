20e38feda52354ea1bd7ecffe5adc882596c7d17
neominal.solutions <neominal.solutions@gmail.com>
2025-09-19 16:22:38 +0300

    son güncellemeler

--- src/services/gateway-service/src/test/java/com/mertalptekin/gatewayservice/SecurityConfig.java
+++ src/services/gateway-service/src/test/java/com/mertalptekin/gatewayservice/SecurityConfig.java
@@ -6,20 +6,18 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+// Gateway'e Token olmadan gelinmez.
+
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http
-                .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/actuator/**").permitAll()
+        http.authorizeExchange(exchanges -> exchanges
                         .anyExchange().authenticated()
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> {})
-                ).csrf(ServerHttpSecurity.CsrfSpec::disable);
+                ).oauth2ResourceServer(oauth2 -> oauth2
+                    .jwt(jwt -> {})).csrf(ServerHttpSecurity.CsrfSpec::disable);
         return http.build();
     }
 }
