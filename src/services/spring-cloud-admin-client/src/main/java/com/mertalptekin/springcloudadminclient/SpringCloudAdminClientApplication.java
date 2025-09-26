56b4541343c787255b3c7a0a2e63081c6de4ec0f
neominal.solutions <neominal.solutions@gmail.com>
2025-09-26 18:04:05 +0300

    Spring Boot Client Admin Entegrasyonu

---
+++ src/services/spring-cloud-admin-client/src/main/java/com/mertalptekin/springcloudadminclient/SpringCloudAdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.mertalptekin.springcloudadminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAdminClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAdminClientApplication.class, args);
+    }
+
+}
